c3f52b26f3eff75b2ae94de3ab27bddfe8ac8d99
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-04-05 13:35:28 +0500

    Update quiz.js

--- test01/quiz.js
+++ test01/quiz.js
@@ -85,7 +85,6 @@ var TextSize=document.getElementById("question-bar");
  }
 
  //Fullscreen
-document.getElementById("full-screen").innerHTML=`<i class="fa fa-compress"></i>`
  function toggleFullScreen() {
 	if (!document.fullscreenElement) {
 	  document.documentElement.requestFullscreen();
