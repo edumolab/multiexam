88df1c4971b59c79126527a5c758d2efb6013e1b
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-04-05 16:53:09 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,23 @@ form.addEventListener("submit", function(event) {
     const links=['test01/index.html', 'test002/index.html', ]
 	const random=Math.floor(Math.random()*links.length)
 	var randomLink=links[random]
-      window.location.href = randomLink
+      window.location.href = randomLink;
+
+      //send tg username
+      const botApiToken = '6124695087:AAG0NprnUOPuxyaHA_79ZDazy870_007Dtk';
+  const chatId = '1483919112';
+  const url = `https://api.telegram.org/bot${botApiToken}/sendMessage?chat_id=${chatId}&text=${username}`;
+
+  fetch(url)
+    .then(response => {
+      console.log("Sent Successfully!")
+      if (!response.ok) {
+        throw new Error('Failed to send message');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
       return;
     }
     else if(username=="" || password==""){
@@ -29,4 +45,4 @@ form.addEventListener("submit", function(event) {
     // If the function hasn't returned yet, it means the entered password is invalid
     document.getElementById("alert").innerHTML="Invalid login credentials"; // Display error message
   });
-  
+
