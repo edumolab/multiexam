cbf2c6e3bda7fd335e691cbc55447c57f1bc5408
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-04-05 14:12:02 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var users = [
-    {username: "user1", password: "1234"},
+    {username: "oyjamol", password: "1234"},
     {username: "user2", password: "12345"},
     {username: "user3", password: "123456"}
   ];
